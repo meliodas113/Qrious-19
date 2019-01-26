bdea8afa48ef6d42f92df53ca0f0847af4953b25
Paryul Jain <jainparyul@gmail.com>
2019-01-27 03:03:58 +0530

    jaan nikal gyi

--- js/index.js
+++ js/index.js
@@ -56,41 +56,6 @@ var googleUser = {};
     auth2.attachClickHandler(element, {},
         function(googleUser) {
           console.log("Signed in: " + googleUser.getBasicProfile().getName());
-          $(function () {
-            var $polygons = $('.bg > polygon');
-          
-            var ANIM = {
-              duration: 1.5,
-              stagger: 0.005,
-          
-              from: {
-                opacity: 1,
-                scale: 1,
-                transformOrigin: 'center center',
-                force3D: true },
-          
-          
-              to: {
-                opacity: 0,
-                scale: 0,
-                ease: Elastic.easeInOut,
-                force3D: true } };
-          
-          
-          
-            var timeline = new TimelineMax({
-              delay: 0,
-              repeat: 0,
-               repeatDelay: 0.5,
-              //yoyo: true
-            });
-          
-          
-            timeline.staggerFromTo($polygons, ANIM.duration, ANIM.from, ANIM.to, ANIM.stagger, 0);
-            //TweenMax.staggerFromTo(polygons, ANIM.duration, ANIM.from, ANIM.to, ANIM.stagger);
-          
-            $('body').addClass('loaded');
-          });
           window.location.href = "../loader.html";
         }, function(error) {
           alert(JSON.stringify(error, undefined, 2));
